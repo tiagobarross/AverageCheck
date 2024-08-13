f7f1fcfdd6ce61159ac72906795ba966a889fd3a
Tiago <tiagobarros519@gmail.com>
2024-08-13 11:06:15 -0300

    list create

---
+++ script.js
@@ -0,0 +1,25 @@
+let students = [
+  {
+    name: "João",
+    note1: 9,
+    note2: 6
+  },
+
+  {
+    name: "Letícia",
+    note1: 6,
+    note2: 8
+  },
+
+  {
+    name: "Tiago",
+    note1: 7,
+    note2: 9
+  },
+
+  {
+    name: "Júlia",
+    note1: 4,
+    note2: 3
+  }
+]
